667646530b90bcd0219edca4895ca131a4f9a5c5
Goran Mržljak <goran.mrzljak@gmail.com>
2023-10-24 15:55:07 +0200

    update

--- br/04-bf/package/bf/reader.rkt
+++ br/04-bf/package/bf/reader.rkt
@@ -4,7 +4,7 @@
 
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
-  (define module-datum `(module bf-mod "../expander-functional.rkt" ,parse-tree))
+  (define module-datum `(module bf-mod bf/expander-functional ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
 
