c0fc48a75393a084d94bc15e4a6cf710c87527de
Goran Mržljak <goran.mrzljak@gmail.com>
2023-10-30 21:56:37 +0100

    update

---
+++ ts/examples-lexer-t/01/input.rkt
@@ -0,0 +1,23 @@
+#lang lexer-t
+
+module-id: rts/lexer
+lexer: rts-lexer
+
+-----
+stuff to lex
+second part
+-----
+IDENT stuff
+IDENT to
+IDENT lex
+IDENT second
+IDENT part
+
+-----
+another stuff to lex
+-----
+IDENT another
+IDENT stuff
+IDENT to
+IDENT lex2
+
--- ts/src/package/rts/lexer/lexer-tests.rkt
+++ ts/src/package/rts/lexer/lexer-tests.rkt
@@ -1,21 +1,22 @@
-#lang racket/base
+#lang lexer-t
 
-(require rts/lexer brag/support)
+module-id: rts/lexer
+lexer: rts-lexer
 
-(module+ test
-  (require rackunit))
+-----
+stuff to lex
+second part
+-----
+IDENT stuff
+IDENT to
+IDENT lex
+IDENT second
+IDENT part
 
-(define (lex str)
-  (apply-port-proc rts-lexer str))
-
-(define (lex-concise str)
-  (define sl-tokens (lex str))
-  (define tokens (map (lambda (slt) (srcloc-token-token slt)) sl-tokens))
-  (for/list ([t (in-list tokens)]
-             #:unless (token-struct-skip? t))
-    (list (token-struct-type t) (token-struct-val t))))
-
-(module+ test
-  (check-equal? (lex-concise "ident1 ident2") '((IDENT "ident1") (IDENT "ident2")))
-  ;(check-equal? 1 2)
-  )
+-----
+another stuff to lex
+-----
+IDENT another
+IDENT stuff
+IDENT to
+IDENT lex
