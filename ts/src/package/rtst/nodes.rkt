33f77dd973910f00b8f8dd107a26a1921116d30b
Goran Mržljak <goran.mrzljak@gmail.com>
2023-11-18 07:50:19 +0100

    update

--- ts/src/package/rtst/nodes.rkt
+++ ts/src/package/rtst/nodes.rkt
@@ -1,14 +1,24 @@
 #lang racket/base
 
+(require racket/contract)
+
 (provide
- n-source-file
  n-variable-statement
  n-variable-declaration-list
  n-variable-declaration
+ n-identifier
  )
 
+(struct n-source-file-struct (statements) #:transparent)
 (define (n-source-file statements)
   (list 'source-file statements))
+(provide
+ n-source-file-struct
+ (contract-out
+  [struct n-source-file-struct
+    ((statements (listof any/c)))])
+ n-source-file
+ )
 
 (define (n-variable-statement export declaration-list)
   (list 'variable-statement export declaration-list))
@@ -18,3 +28,12 @@
 
 (define (n-variable-declaration name [type #f] [initializer #f])
   (list 'variable-declaration name type initializer))
+
+(define (n-identifier name)
+  (list 'identifier name))
+
+(define (n-array-binding-pattern elements)
+  (list 'array-binding-pattern elements))
+
+(define (n-binding-element name [spread #f] [property-name #f])
+  (list 'binding-element name spread property-name))
